4863c05666a85caa8553951f6e70df11a604da3c
Bart van Oort <bart@vanoort.is>
2021-03-03 18:39:27 +0100

    Fixes bug where mllint executable is not executable after installing from pip

--- build/mllint/cli.py
+++ build/mllint/cli.py
@@ -5,4 +5,5 @@ import os
 def mllint() -> int:
   dirname, _ = os.path.split(__file__)
   mllint_exe = os.path.join(dirname, 'mllint-exe')
+  os.chmod(mllint_exe, os.stat(mllint_exe).st_mode | 0o111) # Ensures mllint-exe is executable, equivalent to `chmod +x`
   return subprocess.run([mllint_exe] + sys.argv[1:]).returncode
