33c736814bc5e35f41b461c5e1281c0007f618d0
Bart van Oort <bart@vanoort.is>
2021-04-14 22:09:57 +0200

    rules: fixes descriptions of CI category and ci/use rule

--- linters/ci/rules.go
+++ linters/ci/rules.go
@@ -1,14 +1,26 @@
 package ci
 
-import "github.com/bvobart/mllint/api"
+import (
+	"fmt"
+
+	"github.com/bvobart/mllint/api"
+)
 
 var RuleUseCI = api.Rule{
 	Slug: "use",
 	Name: "Project uses Continuous Integration (CI)",
-	Details: `blablabla CI is important because ...
+	Details: fmt.Sprintf(`This rule checks if your project is using Continuous Integration (CI).
+To learn more about what CI is, does and entails, see the description of category %s
 
-Something something CI providers Gitlab CI, GH Actions, Azure DevOps, Travis CI
+Implementing CI requires picking a CI provider that will run the automated builds and tests. 
+There are many CI providers available and you will have to make your own decision on which fits you best,
+but %s currently recognises four CI providers, namely:
+- [Azure DevOps Pipelines](https://docs.microsoft.com/en-us/azure/devops/pipelines/?view=azure-devops)
+- [GitHub Actions](https://docs.github.com/en/actions)
+- [Gitlab CI](https://docs.gitlab.com/ee/ci/)
+- [Travis CI](https://docs.travis-ci.com/)
 
-Links to documentation`,
+Follow your CI provider's respective 'Getting Started' guide and set your project up with a pipeline to build, test and lint your project.`,
+		"`ci`", "`mllint`"), // TODO: add a ref to a to be implemented rule on how the structure of your pipelines should be.
 	Weight: 1,
 }
