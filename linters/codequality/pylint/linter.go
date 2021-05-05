c4ddaad8bbe5adc9620baf738f4b1ca914442ba0
Bart van Oort <bart@vanoort.is>
2021-05-05 16:21:06 +0200

    linters: scales Pylint score by the amount of messages per reported line of code

--- linters/codequality/pylint/linter.go
+++ linters/codequality/pylint/linter.go
@@ -9,6 +9,10 @@ import (
 	"github.com/bvobart/mllint/utils/markdowngen"
 )
 
+// Maximum number of lines of code per Pylint message reported.
+// Increasing this means that users are expected to have less code smells per line of code.
+const maxLoCperMsg = 10
+
 func NewLinter() api.Linter {
 	return &PylintLinter{}
 }
@@ -27,27 +31,34 @@ func (l *PylintLinter) LintProject(project api.Project) (api.Report, error) {
 	report := api.NewReport()
 	linter := cqlinters.ByType[cqlinters.TypePylint]
 
+	// check whether Pylint is installed so we can actually run it
 	if !linter.IsInstalled() {
 		report.Scores[RuleNoIssues] = 0
 		report.Details[RuleNoIssues] = fmt.Sprint("Error: ", linter, " is not installed, so it could not be run.")
+		return report, nil
 	}
 
+	// check if there are Python files to run Pylint on
+	loc := project.PythonFiles.CountLoC()
+	if loc == 0 {
+		report.Scores[RuleNoIssues] = 100
+		report.Details[RuleNoIssues] = "No Python code was found in the project's repository."
+		return report, nil
+	}
+
+	// actually run Pylint
 	results, err := linter.Run(project.Dir)
 	if err != nil {
 		return report, fmt.Errorf("Pylint failed to run: %w", err)
 	}
 
-	// TODO: find a way to score the specific rule for this CQlinter
-	// TODO: scale this score by the number of total lines of Python code.
-	report.Scores[RuleNoIssues] = 100 - math.Min(100, float64(4*len(results))) // max 25 messages.
-
+	// calculate score. No Pylint messages = 100%, 1 Pylint message per 20 lines of code = 50%, 1 Pylint message per 10 lines of code = 0%
+	report.Scores[RuleNoIssues] = 100 - 100*math.Min(1, float64(len(results)*maxLoCperMsg)/float64(loc))
 	if len(results) == 0 {
 		report.Details[RuleNoIssues] = "Congratulations, Pylint is happy with your project!"
 	} else {
 		report.Details[RuleNoIssues] = fmt.Sprintf("Pylint reported %d issues with your project:\n\n", len(results)) + markdowngen.List(asInterfaceList(results))
 	}
-
-	// TODO: sort all messages by severity before displaying them.
 
 	// TODO: find a solution for multi-line error messages such as those for duplicate code.
 
--- linters/codequality/pylint/rules.go
+++ linters/codequality/pylint/rules.go
@@ -1,10 +1,25 @@
 package pylint
 
-import "github.com/bvobart/mllint/api"
+import (
+	"fmt"
+
+	"github.com/bvobart/mllint/api"
+)
 
 var RuleNoIssues = api.Rule{
 	Slug: "pylint/no-issues",
 	Name: "Pylint reports no issues with this project",
-	Details: `[Pylint](https://pypi.org/project/pylint/) is a static analysis tool for finding generic programming errors.
-This rule asserts that it does not return any errors when running it on all Python files in this project.`,
+	Details: fmt.Sprintf(`[Pylint](https://pypi.org/project/pylint/) is a static analysis tool for finding generic programming errors.
+This rule checks whether Pylint returns any errors when running it on all Python files in this project.
+
+The score for this rule is determined as a function of the number of messages Pylint returns and the lines of Python code that your project has.
+In the ideal case, Pylint does not recognise any code smells in your project, in which case the score is 100%%.
+When there is one Pylint issue for every 20 lines of code, then the score is 50%%.
+When there is one Pylint issue for every 10 lines of code, then the score is 0%%.
+
+More specifically, in pseudocode, %s.
+
+Note that the measured amount of lines of code includes any non-hidden Python files in the repository, including those that are ignored by Pylint.`,
+		"`score = 100 - 100 * min(1, 10 * number of msgs / lines of code)`"),
+	Weight: 1,
 }
