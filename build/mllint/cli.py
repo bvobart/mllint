7108580d84ca42f7eeb6e82e4bc5b54a80c2f0e1
Bart van Oort <bart@vanoort.is>
2021-05-05 12:47:30 +0200

    When rendering Markdown to console, replace inline code blocks with italic yellow text to avoid the ugly blue (or in my case pink) background

--- build/mllint/cli.py
+++ build/mllint/cli.py
@@ -3,10 +3,12 @@ import sys
 import os
 
 def mllint() -> int:
+  """Runs the mllint executable and returns its exit code"""
   dirname, _ = os.path.split(__file__)
   mllint_exe = os.path.join(dirname, 'mllint-exe')
   os.chmod(mllint_exe, os.stat(mllint_exe).st_mode | 0o111) # Ensures mllint-exe is executable, equivalent to `chmod +x`
-  return subprocess.run([mllint_exe] + sys.argv[1:]).returncode
+  return subprocess.run([mllint_exe] + sys.argv[1:], check=False).returncode
 
 def main():
+  """Runs the mllint executable and exits with the exit code it returned"""
   sys.exit(mllint())
